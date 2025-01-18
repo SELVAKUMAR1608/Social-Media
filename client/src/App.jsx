9741663517de60d353a9900fc327175dbd0ff7ca
SELVAKUMAR1608 <bselvazion@gmail.com>
2025-01-18 19:55:25 +0530

    first-commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import LoginPage from './pages/auth/login/LoginPage';
 import SignupPage from './pages/auth/signup/SignupPage';
-import Sidebar from './components/common/SideBar';
+import Sidebar from './components/common/Sidebar';
 import RightPanel from './components/common/RightPanel';
 import NotificationPage from './pages/notification/Notification';
 import ProfilePage from './pages/profile/ProfilePage';
